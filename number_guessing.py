592d24bdbe0b864b968404c764c8c5868759d59f
CalebHieb <103774819+CalebHieb@users.noreply.github.com>
2022-04-25 12:48:57 -0700

    Updated code
    Made additional changes to application to get closer to finishing correctly. Application now successfully accepts input for play_again but does not end application upon "n" input.

--- number_guessing.py
+++ number_guessing.py
@@ -7,7 +7,7 @@ random_number = random.randint(min_number, max_number)
 
 def start_game():
     print("Hello! Welcome to the number guessing game. Can you guess a number between 1 and 10?"  )
-    user_guess = (input(">  "))
+    user_guess = input(">  ")
     number_of_guesses = 5
     while number_of_guesses >= 1:
         try:
@@ -15,40 +15,37 @@ def start_game():
             if user_guess > 10 or user_guess < 1:
                 raise ValueError
         except ValueError:
-            print("That's not a number, try again!")
-            user_guess = (input(">  "))
+            print("That's not a valid guess, try again!")
+            user_guess = input(">  ")
             continue
         else:
             if user_guess > random_number:
                 number_of_guesses -= 1
                 print("That number is too high.  Try again! You have " + str(number_of_guesses) + " guesses left")
-                user_guess = (input(">  "))
+                user_guess = input(">  ")
                 continue
             elif user_guess < random_number:
                 number_of_guesses -= 1
                 print("That number is too low. Try again! You have " + str(number_of_guesses) + " guesses left")
-                user_guess = (input(">  "))
+                user_guess = input(">  ")
                 continue
             else:
-                if number_of_guesses < 1:
-                    print("Sorry, you are out of guesses. Please try again!")
-                    play_again = input("Would you like to play again Y/N?  ")
-                    while True:
-                        if play_again.lower == "n":
-                            print("Game Over. Thanks for playing!")
-                        elif play_again.lower == "y":
-                            start_game()    
-                            continue
-                else:
-                    print("You guessed correctly! Congratulations!")      
-                    play_again = input("Would you like to play again Y/N?  ")
-                    while True:
-                        if play_again.lower == "n":
-                            print("Game Over. Thanks for playing!")
-                            break
-                        elif play_again.lower == "y":
-                            start_game()
-                        
+                print("You guessed correctly! Congratulations!")      
+                play_again = input("Would you like to play again Y/N?  ")
+                if play_again.lower == "n":
+                    print("Game Over. Thanks for playing!")
+                    break
+                else:play_again.lower == "y"
+                start_game()
+    else:
+        print("Sorry, you are out of guesses. Please try again!")
+        play_again = input("Would you like to play again Y/N?  ")
+        if play_again.lower == "n":
+            print("Game Over. Thanks for playing!")
+            break
+        else: play_again.lower == "y"    
+        start_game()    
+                                
 
     
 start_game()    
